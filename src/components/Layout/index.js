2bfb051279806a7c4db3ec15e54f7790e56a65db
papasunnee <papasunnee@gmail.com>
2021-09-03 17:23:27 +0100

    update whatsapp bar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from "react";
+import Link from "next/link";
+import { FaWhatsapp } from "react-icons/fa";
 import NavBar from "../NavBar";
 import Footer from "../Footer";
 import NewsBar from "../NavBar/NewsBar";
@@ -7,6 +9,7 @@ const Index = (props) => {
   return (
     <Fragment>
       <div className="container-fluid p-0">
+        <WhatsappBar />
         <NewsBar />
         <NavBar />
         {props.children}
@@ -16,3 +19,40 @@ const Index = (props) => {
   );
 };
 export default Index;
+
+const WhatsappBar = () => {
+  return (
+    <div className="whatsappBar">
+      <Link href="https://api.whatsapp.com/send/?phone=+2347037857172">
+        <a target="_blank">
+          <FaWhatsapp
+            size={25}
+            style={{ paddingRight: "5px", flexShrink: 0 }}
+          />
+          <span>How can I help you ?</span>
+        </a>
+      </Link>
+      <style jsx>{`
+        .whatsappBar {
+          position: fixed;
+          background-color: #25d366;
+          z-index: 99999;
+          right: 0;
+          top: 40vh;
+        }
+        .whatsappBar:hover {
+          background-color: #064e3b;
+        }
+        .whatsappBar a {
+          color: white;
+          padding: 10px;
+          display: block;
+          text-decoration: none;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+    </div>
+  );
+};
