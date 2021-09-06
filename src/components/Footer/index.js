355909bb80b5e677b9a1a68ce9cbabe6802ea6f9
papasunnee <papasunnee@gmail.com>
2021-09-06 15:53:09 +0100

    update path

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import { NavItem, NavLink } from "reactstrap";
 import Link from "next/link";
 import { flags } from "../Countries/index";
-import { SocialMedia } from "../Navbar" ;
+import { SocialMedia } from "../NavBar" ;
 
 import "./index.scss";
 import { DownloadBrochure, VacancyAlert } from "../About/Download";
