042f57abec86dfb4e3514591003b47c5b8e176bb
papasunnee <papasunnee@gmail.com>
2021-09-07 10:57:50 +0100

    update nextconfig

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS(withSass());
+module.exports = withCSS(withSass({
+     env: {
+      GEOLOCATION_KEY: "f13b370660844a71be282b2c1337aed6",
+      GOOGLE_RECAPTCHA : "6Le0zPAZAAAAAMZdhOAPB3SJFtAa_EpOlPShHgr4",
+    },
+}));
